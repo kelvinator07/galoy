4e7afd5be4809306d2870f25b3b02de9ce6061f4
vindard <17693119+vindard@users.noreply.github.com>
2022-01-05 17:51:40 -0400

    chore(tracing): add user attributes to quiz question resolver span

--- src/graphql/root/mutation/user-quiz-question-update-completed.ts
+++ src/graphql/root/mutation/user-quiz-question-update-completed.ts
@@ -3,6 +3,11 @@ import { onboardingEarn } from "@config/app"
 import { GT } from "@graphql/index"
 
 import UserQuizQuestionUpdateCompletedPayload from "@graphql/types/payload/user-quiz-question-update-completed"
+import {
+  addAttributesToCurrentSpanAndPropagate,
+  SemanticAttributes,
+  ENDUSER_ALIAS,
+} from "@services/tracing"
 
 const UserQuizQuestionUpdateCompletedInput = new GT.Input({
   name: "UserQuizQuestionUpdateCompletedInput",
@@ -16,34 +21,41 @@ const UserQuizQuestionUpdateCompletedMutation = GT.Field({
   args: {
     input: { type: GT.NonNull(UserQuizQuestionUpdateCompletedInput) },
   },
-  resolve: async (_, args, { uid, logger }) => {
-    const { id } = args.input
+  resolve: async (_, args, { uid, domainUser, logger }) =>
+    addAttributesToCurrentSpanAndPropagate(
+      {
+        [SemanticAttributes.ENDUSER_ID]: domainUser?.id,
+        [ENDUSER_ALIAS]: domainUser?.username,
+      },
+      async () => {
+        const { id } = args.input
 
-    if (!onboardingEarn[id]) {
-      return { errors: [{ message: "Invalid input" }] }
-    }
+        if (!onboardingEarn[id]) {
+          return { errors: [{ message: "Invalid input" }] }
+        }
 
-    try {
-      const quizQuestions = await Accounts.addEarn({
-        quizQuestionId: id,
-        accountId: uid,
-        logger,
-      })
-      const question = quizQuestions[0]
+        try {
+          const quizQuestions = await Accounts.addEarn({
+            quizQuestionId: id,
+            accountId: uid,
+            logger,
+          })
+          const question = quizQuestions[0]
 
-      return {
-        errors: [],
-        userQuizQuestion: {
-          question: { id: question.id, earnAmount: question.value },
-          completed: question.completed,
-        },
-      }
-    } catch (err) {
-      return {
-        errors: [{ message: err.message || err.name }],
-      }
-    }
-  },
+          return {
+            errors: [],
+            userQuizQuestion: {
+              question: { id: question.id, earnAmount: question.value },
+              completed: question.completed,
+            },
+          }
+        } catch (err) {
+          return {
+            errors: [{ message: err.message || err.name }],
+          }
+        }
+      },
+    ),
 })
 
 export default UserQuizQuestionUpdateCompletedMutation
